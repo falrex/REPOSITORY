2e7279f3e78750b6afc532f05e9ca29d52e81b3a
unknown <student@.manila.dlsu.edu.ph>
2010-08-27 10:46:19 +0800

    Updated Question

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 ActionController::Routing::Routes.draw do |map|
+  map.resources :questions
+
+  map.resources :questions
+
+  map.resources :questions
+
   map.resources :users
 
   map.resources :users
--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100826204933) do
+ActiveRecord::Schema.define(:version => 20100827055432) do
 
   create_table "choices", :force => true do |t|
     t.string   "value"
@@ -30,6 +30,12 @@ ActiveRecord::Schema.define(:version => 20100826204933) do
   create_table "classlists", :force => true do |t|
     t.integer  "class_id"
     t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "practices", :force => true do |t|
+    t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
