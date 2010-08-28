11ecb733df48b4e9e788038151de073f92ccecbd
unknown <DeeCee@.(none)>
2010-08-28 15:00:09 -0700

    login session

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -9,14 +9,22 @@ class LoginController < ApplicationController
   def authenticate
     value = params[:username]
   
-    login= User.find(:all, :conditions=>"username='"+params[:username]+"'")
+    login= User.find(:all, :conditions=>"username='"+params[:username]+"' and password = '" + params[:password]+"'")
     if(login.length<=0)
       flash[:error] = "Incorrect Username or Password"
       redirect_to :action=>"index"
-    else
-      puts login[0].username
       
-      redirect_to :controller=>"users", :action=>"index"
+      
+    else
+      session[:userid]=login[0].id.to_s
+      if (login[0].category = "Admin")
+        redirect_to :controller=>"users", :action=>"index"
+      elsif (login[0].category = "Teacher")
+        redirect_to :controller=>"users", :action=>"index"
+      elsif (login[0].category = "Student")
+        redirect_to :controller=>"users", :action=>"index"
+      end
+      
     end
     
   end
